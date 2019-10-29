cda98cf1900142fc8958a1c0efe0b3a1814099f4
Martin Berkhoff Jerez <martin.berkhoff@gmail.com>
2019-10-29 20:36:27 -0300

    fix

--- apptagit/lib/main.dart
+++ apptagit/lib/main.dart
@@ -32,6 +32,7 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Material App',
+        initialRoute: 'homealternative',
         routes: {
           'login': (BuildContext context) => LoginPage(),
           'register': (BuildContext context) => RegisterPage(),
