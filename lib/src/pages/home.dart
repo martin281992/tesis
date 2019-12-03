45a2f1eb0d456b58eca16b56d5b26a0f5863f2fe
Martin Berkhoff Jerez <martin.berkhoff@gmail.com>
2019-12-03 14:57:14 -0300

    clean code
    delete fondoApp in home

--- lib/src/pages/home.dart
+++ lib/src/pages/home.dart
@@ -1,16 +1,20 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:apptagit/src/bloc/provider.dart';
 
 class HomePageTagit extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final bloc = Provider.of(context);
+    var data = bloc.email;
+
     return Scaffold(
       appBar: AppBar(
         title: Text('No te dejes cobrar de mas'),
         backgroundColor: Colors.deepPurple,
       ),
-      drawer: _newMenu(context),
+      drawer: _newMenu(context, data),
       body: Stack(
         children: <Widget>[
           //_fondoApp(),
@@ -19,6 +23,7 @@ class HomePageTagit extends StatelessWidget {
             child: Column(
               children: <Widget>[
                 _titulos(),
+
                 //_botonesHome(context),
               ],
             ),
@@ -26,50 +31,6 @@ class HomePageTagit extends StatelessWidget {
         ],
       ),
       // bottomNavigationBar: _buttonNavigatorBar(context),
-    );
-  }
-
-  Widget _fondoApp() {
-    final gradiente = Container(
-      width: double.infinity,
-      height: double.infinity,
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          begin: FractionalOffset(0.0, 0.6),
-          end: FractionalOffset(3.9, 0.0),
-          colors: [
-            Color.fromRGBO(52, 54, 101, 1.0),
-            Color.fromRGBO(35, 37, 57, 1.0),
-          ],
-        ),
-      ),
-    );
-
-    final cajaEstilosa = Transform.rotate(
-      angle: -pi / 3,
-      child: Container(
-        height: 300.0,
-        width: 250.0,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10.0),
-          gradient: LinearGradient(
-            colors: [
-              Color.fromRGBO(236, 98, 188, 1.0),
-              Color.fromRGBO(201, 122, 122, 1.0)
-            ],
-          ),
-        ),
-      ),
-    );
-
-    return Stack(
-      children: <Widget>[
-        gradiente,
-        Positioned(
-          top: -100.0,
-          child: cajaEstilosa,
-        ),
-      ],
     );
   }
 
@@ -102,83 +63,7 @@ class HomePageTagit extends StatelessWidget {
     );
   }
 
-  Widget _buttonNavigatorBar(BuildContext context) {
-    return Theme(
-      data: Theme.of(context).copyWith(
-          canvasColor: Color.fromRGBO(55, 57, 84, 1.0),
-          primaryColor: Colors.pinkAccent,
-          textTheme: Theme.of(context).textTheme.copyWith(
-              caption: TextStyle(color: Color.fromRGBO(116, 117, 152, 1.0)))),
-      child: BottomNavigationBar(items: [
-        BottomNavigationBarItem(
-            icon: Icon(Icons.perm_contact_calendar, size: 30.0),
-            title: Container()),
-        BottomNavigationBarItem(
-            icon: Icon(Icons.graphic_eq, size: 30.0), title: Container()),
-        BottomNavigationBarItem(
-            icon: Icon(Icons.person_add, size: 30.0), title: Container()),
-      ]),
-    );
-  }
-
-  Widget _botonesHome(BuildContext context) {
-    return Table(
-      children: [
-        TableRow(children: [
-          _crearModulo(
-            context,
-            Colors.red,
-            Icons.account_circle,
-            'Perfil',
-          ),
-          _crearModulo(context, Colors.blueAccent, Icons.group_add, 'socios'),
-        ]),
-        TableRow(children: [
-          _crearModulo(context, Colors.purple, Icons.local_car_wash, 'Autos'),
-          _crearModulo(
-              context, Colors.orange, Icons.location_searching, 'Mis Cobros'),
-        ]),
-        TableRow(children: [
-          _crearModulo(context, Colors.pinkAccent, Icons.grain, 'Estadisticas'),
-          _crearModulo(context, Colors.green, Icons.book, 'Reportes'),
-        ]),
-      ],
-    );
-  }
-
-  Widget _crearModulo(
-      BuildContext context, Color color, IconData icon, String texto) {
-    return Container(
-      child: Container(
-        height: 130,
-        margin: EdgeInsets.all(15.0),
-        decoration: BoxDecoration(
-            color: Color.fromRGBO(62, 66, 107, 0.7),
-            borderRadius: BorderRadius.circular(10.0)),
-        child: Column(
-          children: <Widget>[
-            SizedBox(height: 20),
-            CircleAvatar(
-              backgroundColor: color,
-              radius: 30.0,
-              child: Icon(
-                icon,
-                color: Colors.white,
-                size: 30.0,
-              ),
-            ),
-            SizedBox(height: 10),
-            Text(
-              texto,
-              style: TextStyle(color: color),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Drawer _newMenu(BuildContext context) {
+  Drawer _newMenu(BuildContext context, data) {
     return Drawer(
       child: ListView(
         padding: EdgeInsets.zero,
@@ -207,7 +92,7 @@ class HomePageTagit extends StatelessWidget {
             leading: Icon(Icons.pages, color: Colors.deepPurple),
             title: Text('Mis socios'),
             onTap: () {
-              Navigator.pushNamed(context, 'socios');
+              Navigator.pushNamed(context, 'socioscloud');
             },
           ),
           ListTile(
