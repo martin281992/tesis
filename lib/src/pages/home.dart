9251d703deb43f7705f0b583b7f18b6837b2bc9b
Martin Berkhoff Jerez <martin.berkhoff@gmail.com>
2019-12-18 22:54:03 -0300

    testeando

--- lib/src/pages/home.dart
+++ lib/src/pages/home.dart
@@ -1,15 +1,18 @@
 import 'dart:async';
+import 'dart:convert';
 import 'dart:math';
 
 import 'package:apptagit/src/cloudstore/portales.dart';
 import 'package:apptagit/src/cloudstore/portalesService.dart';
+import 'package:apptagit/src/pages/portales.dart';
 //import 'package:apptagit/src/pages/agregar_coordenada.dart';
 import 'package:flutter/material.dart';
 import 'package:apptagit/src/bloc/provider.dart';
 //import 'package:geolocator/geolocator.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:location/location.dart';
-//import 'package:apptagit/src/cloudstore/firestore_service.dart';
+//import 'package:apptagit/src/cloudstore/firestore_service.dart'
 import 'package:apptagit/src/cloudstore/cobros.dart';
 
 class HomePageTagit extends StatefulWidget {
@@ -26,128 +29,22 @@ class _HomePageTagitState extends State<HomePageTagit> {
     var data = bloc.email;
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text('No te dejes cobrar de mas'),
-        backgroundColor: Colors.deepPurple,
-      ),
-      drawer: _newMenu(context, data),
-      body: StreamBuilder(
+        appBar: AppBar(
+          title: Text('No te dejes cobrar de mas'),
+          backgroundColor: Colors.deepPurple,
+        ),
+        drawer: _newMenu(context, data),
+        body: _body());
+  }
+
+  Widget stream(BuildContext context) {
+    return StreamBuilder<List<Portal>>(
         stream: FirestoreService().getPortales(),
         builder: (BuildContext context, AsyncSnapshot<List<Portal>> snapshot) {
-          if (snapshot.hasError || !snapshot.hasData)
-            return CircularProgressIndicator();
-
-          return ListView.builder(
-              itemCount: snapshot.data.length,
-              itemBuilder: (BuildContext context, int index) {
-                Portal portal = snapshot.data[index];
-                var location = new Location();
-
-                location.onLocationChanged().listen(
-                  (LocationData currentLocation) {
-                    //  print('latitud ${currentLocation.latitude}');
-                    // print('Longitud ${currentLocation.longitude}');
-
-                    var locationemit = currentLocation.longitude.toString();
-
-                    StreamBuilder(
-                      stream: FirestoreService().getPortalesCobro(locationemit),
-                      builder: (BuildContext context,
-                          AsyncSnapshot<List<Portal>> snapshot) {
-                        return ListView.builder(
-                            itemCount: snapshot.data.length,
-                            itemBuilder: (BuildContext context, int index) {
-                              Portal portal = snapshot.data[index];
-
-                              for (var i = 0; i < snapshot.data.length; i++) {
-                                var temp = snapshot.data[i];
-                                print(temp.costo);
-                                if (portal.longitud == locationemit) {
-                                  print(
-                                      'portal: ${portal.longitud} = ${locationemit}');
-                                  print("lo lograste emitiendo y captnado");
-                                  final valor = temp.costo;
-
-                                  final dia = 17;
-                                  final mes = 12;
-                                  final categoria = "tag";
-
-                                  Cobros cobro = Cobros(
-                                    categoria: categoria,
-                                    dia: 17,
-                                    mes: 12,
-                                    valor: valor,
-                                  );
-
-                                  // FirestoreService().addCobro(cobro);
-                                }
-                                //print('que tengo aca ${temp}');
-                              }
-                            });
-                        //return ListTile();
-                      },
-                    );
-                  },
-                );
-                return ListTile();
-              });
-        },
-      ),
-
-      /* StreamBuilder(
-                stream: FirestoreService().getPortales(),
-                builder: (BuildContext context, AsyncSnapshot<List<Portal>> snapshot) {
-                  if (snapshot.hasError || !snapshot.hasData)
-                    return CircularProgressIndicator();
-
-                  return ListView.builder(
-                    itemCount: snapshot.data.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      Portal portal = snapshot.data[index];
-
-                      var geolocator = Geolocator();
-                      var locationOptions = LocationOptions(
-                          accuracy: LocationAccuracy.high, distanceFilter: 10);
-
-                      StreamSubscription<Position> positionStream = geolocator
-                          .getPositionStream(locationOptions)
-                          .listen((Position position) {
-                        print(position == null
-                            ? 'Unknown'
-                            : position.latitude.toString() +
-                                ', ' +
-                                position.longitude.toString());
-
-                        var posLat = position.latitude.toString();
-                        var posLon = position.longitude.toString();
-
-                        if (portal.latitud == posLat) {
-                          print('hola coordenada');
-                          print(portal.latitud);
-                        }
-                      });
-                    },
-                  );
-                },
-              ), */
-
-      /* Stack(
-                children: <Widget>[
-                  //_fondoApp(),
-
-                  SingleChildScrollView(
-                    child: Column(
-                      children: <Widget>[
-                        _titulos(),
-
-                        //_botonesHome(context),
-                      ],
-                    ),
-                  )
-                ],
-              ),*/
-      // bottomNavigationBar: _buttonNavigatorBar(context),
-    );
+          if (snapshot.hasData) {
+            print(snapshot.data.length);
+          }
+        });
   }
 
   Widget _titulos() {
@@ -176,6 +73,78 @@ class _HomePageTagitState extends State<HomePageTagit> {
           ],
         ),
       ),
+    );
+  }
+
+  void getData() {
+    final databaseReference = Firestore.instance;
+    databaseReference
+        .collection("portales")
+        .getDocuments()
+        .then((QuerySnapshot snapshot) {
+      snapshot.documents.forEach((f) => print('${f.data}}'));
+    });
+  }
+
+  _body() {
+    return StreamBuilder(
+      stream: FirestoreService().getPortales(),
+      builder: (BuildContext context, AsyncSnapshot<List<Portal>> snapshot) {
+        var count = snapshot.data.length;
+        print(count);
+        if (snapshot.hasError || !snapshot.hasData)
+          return CircularProgressIndicator();
+
+        return ListView.builder(
+            itemCount: snapshot.data.length,
+            itemBuilder: (BuildContext context, int index) {
+              Portal portal = snapshot.data[index];
+              StreamSubscription<LocationData> locationSubscription;
+
+              var location = new Location();
+
+              locationSubscription = location.onLocationChanged().listen(
+                (LocationData currentLocation) {
+                  var locationemit = currentLocation.longitude.toString();
+                  var locationemit2 = currentLocation.latitude.toString();
+
+                  var arr = [];
+
+                  for (var i = 0; i < count; i++) {
+                    var temp = snapshot.data[i];
+
+                    if (locationemit == temp.longitud &&
+                        locationemit2 == temp.latitud) {
+                      //print(temp.nombreP);
+                      //print(temp.costo);
+                      arr.add(temp.longitud);
+                      arr.add(temp.latitud);
+                      print('dentro del if ${arr}');
+
+                      int valor = int.parse(temp.costo);
+
+                      final muyFuture =
+                          Future.delayed(Duration(seconds: 10), () {
+                        Cobros cobro = Cobros(
+                            categoria: 'tag', dia: 5, mes: 12, valor: valor);
+                        //FirestoreService().addCobro(cobro);
+                      });
+                      //FirestoreService().addCobro(cobro))
+
+                      locationSubscription.pause();
+                    }
+                  }
+                  var timer = Timer(Duration(seconds: 60),
+                      () => locationSubscription.resume());
+
+                  //if (locationemit == temp.longitud) {}
+                },
+              );
+
+              // return ListTile();
+              return ListTile();
+            });
+      },
     );
   }
 
@@ -223,3 +192,5 @@ class _HomePageTagitState extends State<HomePageTagit> {
     );
   }
 }
+
+class NumberCreator {}
